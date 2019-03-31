35607d7deb18b1f7a2bceeb4b5d77e6bac76a483
reginawang <rrreggginnna@gmail.com>
2019-03-31 13:28:37 +0100

    edit post

--- server.js
+++ server.js
@@ -37,15 +37,16 @@ for(var i = 0; i < femaleModel.length; i++){
 // app.post('/addData/')
 
 
-app.post('/method', function(req, res){
+app.post('/method', (req, res) =>{
 	console.log("req:               "+ req.body);
 		console.log("res:             "+res.body);	
 
 	if(req.body.key == "apples"){
 		console.log('yayyy');
 		femaleModel[req.body.long][req.body.lat] += req.body.val;
-		res.send(req.body);
 	}
+		res.send(req.body);
+
 });
 
 app.get('/hi', function(req, res){
